45340638bcfe5286473ecdfaa99500ac9f748416
Meitong Li <limeitongpku@163.com>
2022-05-07 11:37:54 -0400

    game levels

--- app/lib/MineMainGame.dart
+++ app/lib/MineMainGame.dart
@@ -9,6 +9,13 @@ class MainGame extends StatefulWidget {
 }
 
 class _MainGameState extends State<MainGame> {
+  int speedLevel = 1;
+  @override
+  void initState() {
+    super.initState();
+    speedLevel = 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,26 +31,98 @@ class _MainGameState extends State<MainGame> {
               children: <Widget>[
                 ElevatedButton(
                   onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                GameLevel()
-                        )
-                    );
+                    setState(() {
+                      speedLevel = 1;
+                    });
                   },
                   style: ElevatedButton.styleFrom(
-                    primary: Colors.pinkAccent,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                    elevation: 15.0,
+                      primary: (speedLevel == 1) ? Colors.deepPurpleAccent : Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      elevation: 15.0,
+                      fixedSize: Size.fromWidth(200)
                   ),
                   child: const Padding(
                     padding: const EdgeInsets.all(15.0),
                     child: Text(
-                      'Start Game',
+                      'Easy',
                       style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      speedLevel = 2;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                      primary: (speedLevel == 2) ? Colors.deepPurpleAccent : Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      elevation: 15.0,
+                      fixedSize: Size.fromWidth(200)
+                  ),
+                  child: const Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Text(
+                      'Medium',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      speedLevel = 3;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                      primary: (speedLevel == 3) ? Colors.deepPurpleAccent : Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      elevation: 15.0,
+                      fixedSize: Size.fromWidth(200)
+                  ),
+                  child: const Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Text(
+                      'Hard',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+
+
+                Padding(
+                    padding: EdgeInsets.all(50),
+                    child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  GameLevel( gameSpeed: speedLevel, )
+                          )
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.pinkAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        elevation: 15.0,
+                        fixedSize: Size.fromWidth(200)
+                    ),
+                    child: const Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Text(
+                        'Start Game',
+                        style: TextStyle(fontSize: 20),
+                      ),
                     ),
                   ),
                 ),
