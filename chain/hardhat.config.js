159198d0b5d13bd95d2d7bbe9201f08170344c38
Trevor Keith <tk@tkeith.com>
2022-05-06 21:44:10 -0400

    contract compiled, deployed, and verified

---
+++ chain/hardhat.config.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.13",
+  defaultNetwork: "matic",
+  networks: {
+    matic: {
+      url: "https://polygon-rpc.com/",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  }
+};
