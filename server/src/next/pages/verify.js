b4046564310abe7c2906f174dbe6b8c2666f6744
Trevor Keith <tk@tkeith.com>
2022-05-07 23:01:04 -0400

    style improvement for verify

--- server/src/next/pages/verify.js
+++ server/src/next/pages/verify.js
@@ -38,8 +38,8 @@ export default function VerifyPage() {
 
   return (
     info ? (
-
-      <div style={{ width: '200px', margin: '100px auto', border: 'solid 1px gray', padding: '20px' }}><div>
+      <div className='bg-gradient-to-t from-cyan-900 to-zinc-700 h-screen text-white'>
+      <div style={{ width: '200px', margin: '0 auto', border: 'solid 1px gray', padding: '20px' }}><div>
         <a href={'https://ipfs.io/ipfs/' + info.ipfsHash}>Audio file</a>
         <div style={{ height: '20px' }} />
         <div> <TextButton onClick={() => complete(info.taskId, info.submissionId, info.options[0])}>{info.options[0]}</TextButton> </div>
@@ -49,7 +49,10 @@ export default function VerifyPage() {
         <div> <TextButton onClick={() => complete(info.taskId, info.submissionId, info.options[2])}>{info.options[2]}</TextButton> </div>
         <div style={{ height: '20px' }} />
         <div> <TextButton onClick={() => complete(info.taskId, info.submissionId, info.options[3])}>{info.options[3]}</TextButton> </div>
-      </div></div>
-    ) : <div style={{ width: '100px', margin: '100px auto' }} > <div>Loading...</div></div>
+        </div></div></div>
+    ) :
+      <div className='bg-gradient-to-t from-cyan-900 to-zinc-700 h-screen text-white'>
+        <div style={{ width: '100px', margin: '0 auto', padding: '50px' }} > <div>Loading...</div></div>
+      </div>
     )
 }
