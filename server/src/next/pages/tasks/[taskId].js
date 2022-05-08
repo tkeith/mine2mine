df8bea3e18ba0e7d619edbeae33bea49a866b6bc
“Mark <markrodriguez@Marks-MacBook-Air.local>
2022-05-07 21:05:33 -0400

    taskid submission fix

--- server/src/next/pages/tasks/[taskId].js
+++ server/src/next/pages/tasks/[taskId].js
@@ -1,24 +1,37 @@
 import { useRouter } from 'next/router'
 import { taskInfo } from '..'
+import { useEffect, useState } from "react";
+
 
 const TaskPage = () => {
   const router = useRouter()
   const { taskId } = router.query
   
-  async function getSubmissionData() {
-    const userData = await fetch( 'express/tasks/'+{taskId}+'/submissions', {
-      method: 'GET'
-    })
-    console.log(userData)
-    return await userData
-  }
+  let [info, setInfo] = useState({subs: []})
+  useEffect(function () {
+    (async () => {
+      //alert('getting taskId: ' + taskId)
+      const res = await (await fetch('https://mine2mine.tk.co/express/tasks/'+ taskId + '/submissions', {
+        method: 'GET'
+      })).json()
+      // const taskId = res.taskId
+      // const submissionId = res.submissionId
+      // const creator = res.creator
+      // const ipfsHash = res.ipfsHash
+  
+      setInfo({subs: res})
+    })()
+  }, [])
+
+  const rows = info.subs.map((sub) => 
+  <p><span>{sub.creator}</span><span>{sub.ipfsHash}</span></p>)
 
   return (
     <div className='bg-gradient-to-t from-cyan-900 to-zinc-700 h-screen text-white'>
 
         <div className=' text-center'>
             <h1>Task Number: {taskId}</h1>
-            <h1>Audio Submissions:{}</h1>
+            {rows}
         </div>
       
     </div>
