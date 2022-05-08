8abb61ec4ed54c12458fee7534c7cc25c48353f2
Trevor Keith <tk@tkeith.com>
2022-05-07 23:52:54 -0400

    privacy & marketing URLs for Apple

--- server/src/next/next.config.js
+++ server/src/next/next.config.js
@@ -12,6 +12,16 @@ module.exports = {
         destination: 'https://www.canva.com/design/DAFACDQMrJc/ptGIY1cMy1OyHinTAn7OJw/view?utm_content=DAFACDQMrJc&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink',
         permanent: false,
       },
+      {
+        source: '/privacy',
+        destination: 'https://docs.google.com/document/d/16NFZy5OKtvAJdlUKppS5SkBsjCxotKedCdHMIs36jCU/edit?usp=sharing',
+        permanent: false,
+      },
+      {
+        source: '/marketing',
+        destination: 'https://m2m.tk.co/',
+        permanent: false,
+      },
     ]
   },
 }
