10c5d77f3115d5eaa98c506acaea0124074f9f7b
Trevor Keith <tk@tkeith.com>
2022-05-07 23:39:49 -0400

    pres link

--- server/src/next/next.config.js
+++ server/src/next/next.config.js
@@ -7,6 +7,11 @@ module.exports = {
         destination: 'https://google.com/',
         permanent: false,
       },
+      {
+        source: '/presentation',
+        destination: 'https://www.canva.com/design/DAFACDQMrJc/ptGIY1cMy1OyHinTAn7OJw/view?utm_content=DAFACDQMrJc&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink',
+        permanent: false,
+      },
     ]
   },
 }
